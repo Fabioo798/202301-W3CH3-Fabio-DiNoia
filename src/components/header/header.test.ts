642d0a3b79bc4fdef19e896534803b501e0a09b1
Fabio <fabiodn.798@gmail.com>
2023-02-01 20:55:58 +0100

    Modify header.test description

--- src/components/header/header.test.ts
+++ src/components/header/header.test.ts
@@ -5,13 +5,13 @@ import { Header } from './header';
 
 describe('Given Header component', () => {
   describe('When new Header is instanced', () => {
-    test('Then it should instance a new header class', () => {
+    test('Then it should create a new header class', () => {
       document.body.innerHTML = `<slot></slot>`;
       const mockTitle = 'Test';
       const element = new Header('slot', mockTitle);
       expect(element).toBeInstanceOf(Header);
     });
-    
+
     test('Then it should render a new template', () => {
       const h1 = screen.getByRole('heading');
       expect(h1).toBeInTheDocument();
